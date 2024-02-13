91c32e19fc2a8b2ed522a8d6b881118bb19053d7
1wolftman <158086578+1wolftman@users.noreply.github.com>
2024-02-13 22:47:02 +0300

    Update kayit.php

--- kayit.php
+++ kayit.php
@@ -3,7 +3,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $kullaniciAdi = $_POST["kullaniciAdi"];
     $sifre = $_POST["sifre"];
 
-    $dosya = fopen("kullanicilar.txt", "a"); // "a" modu dosyayı yazmak için açar, dosya yoksa oluşturur
+    $dosya = fopen("kullanici_bilgileri.txt", "a"); // "a" modu dosyayı yazmak için açar, dosya yoksa oluşturur
     fwrite($dosya, "Kullanıcı Adı: " . $kullaniciAdi . ", Şifre: " . $sifre . "\n");
     fclose($dosya);
 }
