83ef0d821d0405d69f063fe4cd7f57f4b808d528
1wolftman <158086578+1wolftman@users.noreply.github.com>
2024-02-15 21:24:24 +0300

    Update veri_gonder.php

--- veri_gonder.php
+++ veri_gonder.php
@@ -1,6 +1,6 @@
 <?php
 // Discord botunuzun token'ı
-$bot_token = "MTIwNzc1MDA5MDcxOTEwMDkzOQ.G-861H.A4Lps1Yi-NcYI1JeiEcmWNvhd_D7xacEUh24Fs";
+$bot_token = "MTIwNzc1MDA5MDcxOTEwMDkzOQ.GO-eKZ.s6pe9s2lEqlmvxRxUraEr84nKG3FMZ1A81IrLM";
 
 // Göndermek istediğiniz sunucunun ID'si
 $server_id = "1207747519514415124";
